b4da5c16ea967663c7488a0767c3cd5a30212976
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-09-15 11:18:11 +0400

    created articles page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 
 Route::get('contact', 'PagesController@contact');
 Route::get('about', 'PagesController@about');
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
