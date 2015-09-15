46f225fba6ea4ac6a60e816fa8dccb888b155a60
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-09-15 15:15:50 +0400

    create article validation

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Http\Requests\CreateArticleRequest;
 use Carbon\Carbon;
-use Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class ArticlesController extends Controller
 {
+    /**
+     * Show all articles.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
         $articles = Article::latest('published_at')->published()->get();
@@ -18,6 +23,12 @@ class ArticlesController extends Controller
         return view('articles/index', compact('articles'));
     }
 
+    /**
+     * Show a single article.
+     *
+     * @param $id
+     * @return \Illuminate\View\View
+     */
     public function show($id)
     {
         $article = Article::findOrFail($id);
@@ -25,14 +36,31 @@ class ArticlesController extends Controller
         return view('articles/show', compact('article'));
     }
 
+    /**
+     * Show the page to create a new article.
+     *
+     * @return \Illuminate\View\View
+     */
     public function create()
     {
         return view('articles/create', compact('article'));
     }
 
-    public function store()
+    /**
+     * Save a new article.
+     *
+     * @param CreateArticleRequest $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(CreateArticleRequest $request)
     {
-        Article::create(Request::all());
+        /*
+        store(Request $request)
+        2 method
+        $this->validate($request, ['title' => 'required', 'body' => 'required']);
+        */
+
+        Article::create($request->all());
 
         return redirect('articles');
     }
